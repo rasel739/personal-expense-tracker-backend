60cdf898c65578406feef98e436e9c4c550ec9bb
rasel739 <raselhossaintro@gmail.com>
2025-10-24 15:03:13 +0600

    config file change

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join(process.cwd(), '.env'), quiet: true });
 
 export default {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: process.env.PORT || 5000,
   database_url: process.env.DATABASE_URL,
   bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt: {

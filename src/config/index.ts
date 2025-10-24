ec81d462ac046d2bddebb7e94dacec5259630147
rasel739 <raselhossaintro@gmail.com>
2025-10-24 17:03:10 +0600

    vercel json and config file change

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,9 +7,9 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT || 5000,
   database_url: process.env.DATABASE_URL,
-  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  bycrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || '10',
   jwt: {
-    secret: process.env.JWT_SECRET,
-    expires_in: process.env.JWT_EXPIRES_IN,
+    secret: process.env.JWT_SECRET || 'd760c0868d0fb14ca0ba0878b0f85722',
+    expires_in: process.env.JWT_EXPIRES_IN || '7d',
   },
 };
